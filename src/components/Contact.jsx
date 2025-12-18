526d93cd7729d8779b84c1838337d7c0f938c481
nygel-ui <kyledheyvid@gmail.com>
2025-12-19 01:16:32 +0800

    details - commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,27 +11,27 @@ const Contact = () => {
             <h2 className="text-xl sm:text-2xl md:text-4xl font-bold text-gray-300 mb-4 md:mb-8">GET IN TOUCH</h2>
             <hr className="w-full border-t-2 border-gray-600 opacity-50 mb-4 md:mb-8" />
             <div className="flex space-x-4 md:space-x-10">
-                <a href="https://github.com/walts-14" target="_blank" rel="noopener noreferrer" className="w-12 h-12 md:w-16 md:h-16 rounded-full flex items-center justify-center text-4xl text-white hover:bg-blue-400 transition">
+                <a href="https://github.com/nygel-ui" target="_blank" rel="noopener noreferrer" className="w-12 h-12 md:w-16 md:h-16 rounded-full flex items-center justify-center text-4xl text-white hover:bg-blue-400 transition">
                     <img src={github} alt="GitHub" className="w-10 h-10 md:w-15 md:h-15" />
                 </a>
-                <a href="https://linkedin.com/in/johnewalter-almcd" target="_blank" rel="noopener noreferrer" className="w-12 h-12 md:w-16 md:h-16 rounded-full flex items-center justify-center text-4xl text-white hover:bg-blue-400 transition">
+                <a href="https://www.linkedin.com/in/dheyvid-kyle-pe-2a296436a/" target="_blank" rel="noopener noreferrer" className="w-12 h-12 md:w-16 md:h-16 rounded-full flex items-center justify-center text-4xl text-white hover:bg-blue-400 transition">
                     <img src={linkedin} alt="LinkedIn" className="w-10 h-10 md:w-15 md:h-15" />
                 </a>
-                <a href="https://www.facebook.com/johnewalter.alcala/" target="_blank" rel="noopener noreferrer" className="w-12 h-12 md:w-16 md:h-16 rounded-full flex items-center justify-center text-4xl text-white hover:bg-blue-400 transition">
+                <a href="https://www.facebook.com/dheyvidkyle.pe" target="_blank" rel="noopener noreferrer" className="w-12 h-12 md:w-16 md:h-16 rounded-full flex items-center justify-center text-4xl text-white hover:bg-blue-400 transition">
                     <img src={facebook} alt="Facebook" className="w-10 h-10 md:w-15 md:h-15" />
                 </a>
             </div>
             <div className="flex flex-col md:flex-row items-center justify-center text-sm md:text-base text-gray-400 mt-4 md:mt-6 space-y-2 md:space-y-0 md:space-x-6">
                 <div className="flex items-center space-x-2">
                     <img src={contact} alt="Phone" className="w-8 h-8 md:w-10 md:h-10" />
-                    <span>+63 955-750-1035</span>
+                    <span>+63 968 397 4705</span>
                 </div>
                 <div className="flex items-center space-x-4">
                     <img src={email} alt="Email" className="w-9 h-9 md:w-11 md:h-11" />
-                    <span>johnewalteralmcd@gmail.com</span>
+                    <span>kyledheyvid@gmail.com</span>
                 </div>
             </div>
-            <p className="text-gray-300 mt-6 md:mt-10">© 2025 Johne Walter Almonacid</p>
+            <p className="text-gray-300 mt-6 md:mt-10">© 2025 Dheyvid Kyle Pe</p>
         </section>
     );
 };
