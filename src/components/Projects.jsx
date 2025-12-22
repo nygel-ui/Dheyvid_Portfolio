18bedaaa79f7a7e29cdf74336e0d057773a8b383
walts-14 <walterjohne14@gmail.com>
2025-12-23 00:31:03 +0800

    projects replace

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const categories = [
                     'Photoshop', 
                     // 'Illustrator', 
                     'Premiere Pro', 
-                    'After Effects',
+                    // 'After Effects',
                     // 'Figma', 
                     // 'Code'
                     
