3eaefb456d778aaa5bf169bc9fec2717763bfb32
nygel-ui <kyledheyvid@gmail.com>
2025-12-24 01:32:04 +0800

    other details

--- src/components/Intership.jsx
+++ src/components/Intership.jsx
@@ -15,7 +15,7 @@ const Intership = () => {
                     <img src={essensaLogo} alt="Essensa Naturale logo" className="w-40 h-auto object-contain" />
                 </div>
 
-                <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white mb-4 md:mb-6">About Toyota Marilao Bulacan I</h1>
+                <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white mb-4 md:mb-6">About Toyota Marilao Bulacan</h1>
 
                 <p className="text-gray-300 text-base sm:text-lg md:text-xl leading-relaxed">
                    Toyota Marilao, Bulacan is a major authorized Toyota car dealership and service center located along MacArthur Highway in Marilao, Bulacan. It offers new Toyota vehicles, sales support, and comprehensive after-sales services including maintenance, repairs, and appointments, all delivered with a focus on customer satisfaction and quality care. The dealership is also known for embracing environmentally friendly design and modern, energy-efficient facilities as part of its commitment to sustainable automotive service.
